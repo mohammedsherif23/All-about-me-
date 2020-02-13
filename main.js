27944aebb366e366fd220588f8c12410ebfe3d64
Mohammed Sherif <sherifmohammed634@gmail.com>
2020-02-13 20:11:42 +0800

    added team gallery to my hobbies

---
+++ main.js
@@ -0,0 +1,9 @@
+new Swiper(".swiper-container", {
+    speed: 400,
+    spaceBetween: 100,
+    effect: "flip",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+});
